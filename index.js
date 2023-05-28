f747dfd64b7ce1fc9e1b7c1c2bf1f6877930b16f
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-05-28 18:02:36 -0400

    added code to generate engineer and intern profile

--- index.js
+++ index.js
@@ -58,9 +58,45 @@ function addEngineer(){
         message: 'What is their gitHub profile username?'
     },
 ]).then(answers => {
-    const engineer = new Engineer(answers.engineerName, answers.EngineerEmail, )
+    const engineer = new Engineer(answers.engineerName, answers.engineerEmail, answers.engineerGithub);
+    teamMembers.push(engineer);
+    buildTeam();
 })
 }
-};
+
+function addIntern() {
+    inquirer.prompt([
+      
+      {
+        type: "input",
+        name: "internName",
+        message: "What is the intern's name?"
+      },
+
+      {
+        type: "input",
+        name: "internId",
+        message: "What is the intern's employee ID number?" 
+      },
+
+      {
+        type: "input",
+        name: "internEmail",
+        message: "What is the intern's email address?"
+      },
+
+      {
+        type: "input",
+        name: "internSchool",
+        message: "What school does the intern attend?"
+      }
+
+    ]).then(answers => {
+      const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+        teamMembers.push(intern);
+        buildTeam();
+    });
+
+}
 
 buildTeam();
---
+++ src/generate.js
@@ -0,0 +1,11 @@
+const buildTeam = team => {
+    const generateManager = manager => {
+        return `
+        <div class="profile-card" id="manager-display">
+        
+        </div>
+        
+        
+        `
+    }
+}
