8f767ed21c731a2672a7bc7e6d2b3b9210919115
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-05-30 08:08:18 -0400

    updated code

--- src/generate.js
+++ src/generate.js
@@ -1,49 +1,49 @@
 const createTeam = team => {
     const generateManager = manager => {
         return `
-        <div class="employee-card" id="manager-card">
-    <div class="header" id="manager-header">
-        <h2 class="card-title">${manager.getName()}</h2>
-        <h3 class="card-title">${manager.getRole()}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li>ID: ${manager.getId()}</li>
-            <li>Email:${manager.getEmail()}</li>
-            <li>Office number: ${manager.getOfficeNumber()}</li>
-        </ul>
-    </div>
-</div>
+        <div class="card" id="manager-card">
+        <div class="card-header" id="manager">
+            <h2>${manager.getName()}</h2>
+            <h3>${manager.getRole()}</h3>
+        </div>
+        <div class="list">
+            <ul class="list-details">
+                <li>ID: ${manager.getId()}</li>
+                <li>Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                <li>Office Number: ${manager.getOfficeNumber()}</li>
+            </ul>
+        </div>
+    </div>  
         `;
     };
     const generateEngineer = engineer => {
         return `
-        <div class="employee-card" id="engineer-card">
-    <div class="header" id="engineer-header">
-        <h2 class="card-title">${engineer.getName()}</h2>
-        <h3 class="card-title">${engineer.getRole()}</h3>
+    <div class="card" id="engineer-card">
+        <div class="card-header" id="engineer">
+            <h2>${engineer.getName()}</h2>
+            <h3>${engineer.getRole()}</h3>
+        </div>
+        <div class="list">
+            <ul class="list-details">
+                <li>ID:  ${engineer.getId()}</li>
+                <li>Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                <li>Github: <a href="https://github.com/${engineer.getGithub()}">${engineer.getGithub()}</a></li>
+            </ul>
+        </div>
     </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li>ID: ${engineer.getId()}</li>
-            <li>Email:${engineer.getEmail()}</li>
-            <li class="list-group-item">GitHub: ${engineer.getGithub()}</li>
-        </ul>           
-    </div>
-</div>
         `; 
     };
 const generateIntern = intern => {
         return `
-    <div class="employee-card" id="intern-card>
-        <div class="header">
-            <h2 class="card-title">${intern.getName()}</h2>
-            <h3 class="card-title">${intern.getRole()}</h3>
+        <div class="card" id="intern-card">
+        <div class="card-header" id="intern">
+            <h2>${intern.getName()}</h2>
+            <h3>${intern.getRole()}</h3>
         </div>
-        <div class="card-body">
-            <ul class="list-group">
+        <div class="list">
+            <ul class="list-details">
                 <li>ID: ${intern.getId()}</li>
-                <li>Email:${intern.getEmail()}</li>
+                <li>Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
                 <li>School: ${intern.getSchool()}</li>
             </ul>
         </div>
@@ -74,30 +74,25 @@ const generateIntern = intern => {
 module.exports = team => {
 
     return `
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-    <div class= container>
-        <div class=>           
-                <h1 class="text-center text-white">My Team</h1>
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="stylesheet" href="../src/assets/css/style.css">
+    </head>
+    
+    <body>
+        <div class="banner">           
+            <h1>My Team</h1>
         </div>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="team">
+        
+        <div class="container">
                 ${createTeam(team)}
-            </div>
-        </div>
-    </div>
-</body>
+        </div>   
+   </body>
 </html>
     `;
 };
